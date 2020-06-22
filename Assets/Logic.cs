7e455452fb7582c68a6d92a84ef61dea9676d0b1
Umut zenger <zengerumut@gmail.com>
2020-06-22 13:15:41 +0300

    init

---
+++ Assets/Logic.cs
@@ -0,0 +1,61 @@
+﻿/*  This script was created by:
+ *  Umut Zenger
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    #region Public Members
+
+    #endregion
+
+    #region Private Members
+
+    private bool[,] maze;
+    [SerializeField]
+    private GameObject wall;
+    [SerializeField]
+    private Transform platform, wallParent;
+    [SerializeField]
+    private int width, height;
+    [SerializeField]
+    private float density, complexity;
+    private float time;
+
+    #endregion
+    
+    #region Public Functions
+    
+    #endregion
+    
+    #region Private Functions
+    void Start()
+    {
+        maze = MazeGenerator.GenerateMaze(width, height, density, complexity);
+        time = Time.time;
+        print($"Generated maze in {time} seconds.");
+        CreateMaze();
+    }
+
+    void CreateMaze()
+    {
+        wallParent.position = new Vector3((maze.GetLength(1) - 1) / 2, 0, (maze.GetLength(0) - 1) / 2);
+        platform.localScale = new Vector3(maze.GetLength(1) / 10, 1, maze.GetLength(0) / 10);
+        for(int i = 0; i < maze.GetLength(0); i++)
+        {
+            for(int j = 0; j < maze.GetLength(1); j++)
+            {
+                if (maze[i, j])
+                {
+                    Instantiate(wall, new Vector3(j, 2f, i), Quaternion.identity, wallParent);
+                }
+            }
+        }
+        wallParent.position = new Vector3(0, 0, 0);
+        print($"Created maze in {Time.time - time} seconds.");
+    }
+    #endregion
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿/*  This script was created by:
+ *  Umut Zenger
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Public Members
+
+    #endregion
+
+    #region Private Members
+    [SerializeField]
+    private float speed = 1;
+    #endregion
+    
+    #region Public Functions
+    
+    #endregion
+    
+    #region Private Functions
+    
+    void FixedUpdate()
+    {
+        transform.RotateAround(transform.position, transform.up, Input.GetAxis("Horizontal")*Time.deltaTime * 90f);
+        GetComponent<Rigidbody>().velocity = (transform.forward * Input.GetAxis("Vertical") * speed);
+        //GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical"));
+    }
+    #endregion
+}
